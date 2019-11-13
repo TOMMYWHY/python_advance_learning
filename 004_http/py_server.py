d06623d304cd50a47e9deb858da5f6b6e13c6aa7
tommy <tommywhy1989@gmail.com>
2019-11-13 23:03:20 +1300

    simple server v4 单进程-单线程-非阻塞

--- 004_http/py_server.py
+++ 004_http/py_server.py
@@ -6,8 +6,8 @@ from gevent import monkey
 monkey.patch_all()
 
 
-def service_client(new_socket, client_addr):
-    request = new_socket.recv(1024).decode("utf-8")
+def service_client(new_socket, request):
+    # request = new_socket.recv(1024).decode("utf-8")
     request_lines = request.splitlines()
     file_name = ""
     # ['GET /index HTTP/1.1', 'Host: 192.168.88.102:7890', 'Connection: keep-alive', 'Cache-Control: max-age=0', 'Upgrade-Insecure-Requests: 1', 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36', 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3', 'Accept-Encoding: gzip, deflate', 'Accept-Language: en-NZ,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-GB;q=0.6,en-US;q=0.5', '']
@@ -30,23 +30,48 @@ def service_client(new_socket, client_addr):
     else:
         content = f.read()
         f.close()
-        response = "HTTP/1.1 200 OK \r\n"  # 协议头
-        response += "\r\n"  # 空行
-        new_socket.send(response.encode("utf-8"))  # 转码
-        new_socket.send(content)  # 发送 body
-        new_socket.close()
+
+        response_body = content
+        response_header = "HTTP/1.1 200 OK \r\n"  # 协议头
+        response_header += "Content-Length: %d \r\n" % len(response_body)  # 设置长度，以便于长连接的结束
+        response_header += "\r\n"  # 空行
+        response = response_header.encode("utf-8") + response_body
+
+        new_socket.send(response)  # 发送 body
+
+        # new_socket.close()
 
 
 def main():
     # create server side socket
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_socket.bind(("", 7890))
+    tcp_socket.setblocking(False)  # 设置非阻塞
     tcp_socket.listen(128)
 
+    client_socket_list = list()  # 单线程 非阻塞
     while True:
-        new_socket, client_addr = tcp_socket.accept()
+        try:
+            new_socket, client_addr = tcp_socket.accept()
+        except Exception as ret:
+            print("no one connect yet...")
+        else:
+            new_socket.setblocking(False)
+            client_socket_list.append(new_socket)
+
+        for client_socket in client_socket_list:
+            try:
+                recv_data = client_socket.recv(1024).decode("utf-8")
+            except Exception as ret:
+                print("not receive yet...")
+            else:
+                if recv_data:
+                    service_client(client_socket, recv_data)
+                else:
+                    client_socket.close()  # 断开 connect
+                    client_socket_list.remove(client_socket)  # 移除
         # 创建协程
-        gevent.spawn(service_client, new_socket, client_addr)
+        # gevent.spawn(service_client, new_socket, client_addr)
 
         # service_client(new_socket, client_addr)
 
