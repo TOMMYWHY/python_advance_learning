3821db0f111ef67ec61c5f5e022cdfe984ec009b
tommy <tommywhy1989@gmail.com>
2019-11-13 21:07:19 +1300

    simple server v3 gevent

--- 004_http/py_server.py
+++ 004_http/py_server.py
@@ -1,10 +1,15 @@
 import socket
 import re
+import gevent
+from gevent import monkey
+
+monkey.patch_all()
 
 
 def service_client(new_socket, client_addr):
     request = new_socket.recv(1024).decode("utf-8")
     request_lines = request.splitlines()
+    file_name = ""
     # ['GET /index HTTP/1.1', 'Host: 192.168.88.102:7890', 'Connection: keep-alive', 'Cache-Control: max-age=0', 'Upgrade-Insecure-Requests: 1', 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36', 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3', 'Accept-Encoding: gzip, deflate', 'Accept-Language: en-NZ,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-GB;q=0.6,en-US;q=0.5', '']
     ret = re.match(r"[^/]+(/[^ ]*)", request_lines[0])
     # re: 从头开始^匹配多次+到/   /   从头开始^匹配到空格 可以0-多次
@@ -15,7 +20,7 @@ def service_client(new_socket, client_addr):
 
     # 读取 html
     try:
-        f = open("./cv-blog"+file_name, "rb")
+        f = open("./cv-blog" + file_name, "rb")
     except:
         response = "HTTP/1.1 404 NOT FOUND\r\n"
         response += "\r\n"
@@ -32,8 +37,6 @@ def service_client(new_socket, client_addr):
         new_socket.close()
 
 
-
-
 def main():
     # create server side socket
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -42,7 +45,10 @@ def main():
 
     while True:
         new_socket, client_addr = tcp_socket.accept()
-        service_client(new_socket, client_addr)
+        # 创建协程
+        gevent.spawn(service_client, new_socket, client_addr)
+
+        # service_client(new_socket, client_addr)
 
     tcp_socket.close()  # 关闭 socket。。。
 
